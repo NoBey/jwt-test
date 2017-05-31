c17af629d6ff3ba96b12304236facb9fd3adb46c
luck <616646651@qq.com>
2017-05-31 17:59:51 +0800

    ok

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ var jwt = require('koa-jwt');
 
 var app = new koa();
 
+
+app.use(function(ctx, next){
+  if (ctx.url.match(/^\/p/)) {
+    ctx.body = '请求头 Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIyMjIiLCJpYXQiOjE0OTYyMjM4NzN9.HUD8F3WJPVxPwp1M_wlpyKbjqBCGxbECetXUo-r_H9g\n';
+  } else {
+    return next();
+  }
+});
+
+
 app.use(jwt({
   secret: 'nobey1',
   // key: 'jwtdata'
 }));
-
 // curl --header "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIyMjIiLCJpYXQiOjE0OTYyMjM4NzN9.HUD8F3WJPVxPwp1M_wlpyKbjqBCGxbECetXUo-r_H9g" 127.0.0.1:3001
 // 请求头 Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIyMjIiLCJpYXQiOjE0OTYyMjM4NzN9.HUD8F3WJPVxPwp1M_wlpyKbjqBCGxbECetXUo-r_H9g
 
